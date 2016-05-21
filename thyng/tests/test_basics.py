a57dd5c29b88c62045a519b4c62b6dc56f6cb3bc
Ethan Jucovy <ethan.jucovy@gmail.com>
2016-05-21 11:44:41 -0400

    Testing travis

--- thyng/tests/test_basics.py
+++ thyng/tests/test_basics.py
@@ -3,4 +3,4 @@ from django.test import TestCase, override_settings
 class BasicTest(TestCase):
 
     def test_foo(self):
-        self.assertEquals("foo", "bar")
+        self.assertEquals("foo", "foo")
